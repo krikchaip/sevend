00747bb69df2cd0993b58562011a76ac2f22ce7f
krikchaip <krikchai.p@gmail.com>
2018-06-08 18:07:41 +0700

    👁 base layout structure

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,11 +1,45 @@
 <template>
+  <v-app class="unselectable">
+    <v-toolbar
+      app
+      clipped-left
+      dark
+      color="info"
+      height="56px"
+    >
+      <v-toolbar-side-icon @click="menu = !menu">
+        <v-icon>mdi-menu</v-icon>
+      </v-toolbar-side-icon>
+      <v-toolbar-title>Menu Title</v-toolbar-title>
+      <v-spacer/>
+    </v-toolbar>
+    <v-navigation-drawer
+      app
+      clipped
+      :permanent="menu"
+    >
+    </v-navigation-drawer>
+    <v-content>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, aliquid.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
+    </v-content>
+  </v-app>
 </template>
 
 <script>
   export default {
-
+    name: 'App',
+    data: () => ({
+      menu: false
+    })
   }
 </script>
 
 <style>
+  @import url('assets/styles.css');
+  @import url('/node_modules/@mdi/font/css/materialdesignicons.min.css');
 </style>
