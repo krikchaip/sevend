6f00ab61063d8164f5e308dfe5f6b0ffa66b2417
krikchaip <krikchai.p@gmail.com>
2018-06-08 18:55:36 +0700

    👁 base layout structure : fixed drawer z-index issue

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,11 +1,21 @@
 <template>
   <v-app class="unselectable">
+    <v-navigation-drawer
+      app
+      clipped
+      fixed
+      width="250"
+      style="z-index: 3"
+      :permanent="menu"
+    >
+    </v-navigation-drawer>
     <v-toolbar
       app
       clipped-left
       dark
+      fixed
       color="info"
-      height="56px"
+      height="56"
     >
       <v-toolbar-side-icon @click="menu = !menu">
         <v-icon>mdi-menu</v-icon>
@@ -13,19 +23,10 @@
       <v-toolbar-title>Menu Title</v-toolbar-title>
       <v-spacer/>
     </v-toolbar>
-    <v-navigation-drawer
-      app
-      clipped
-      :permanent="menu"
-    >
-    </v-navigation-drawer>
     <v-content>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque, aliquid.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, odio!
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, itaque?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, accusantium?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, adipisci!
     </v-content>
   </v-app>
 </template>
