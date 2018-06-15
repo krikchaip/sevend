4833a11e1b39be097b1deef1bf387956a6cd192e
krikchaip <krikchai.p@gmail.com>
2018-06-16 01:52:42 +0700

    🏗 global : reset userData every restart (ease debugging)

--- src/main/global.js
+++ src/main/global.js
@@ -2,11 +2,12 @@ import Store from 'electron-store'
 
 // Managing user data across all processes
 global.data = new Store({
-  defaults: {
-    appProjects: []
-  },
+  defaults: { appProjects: [] },
   name: 'data',
-  encryptionKey:
-    process.env.NODE_ENV !== 'development'
-      ? 'data-secret' : undefined
+  encryptionKey: 'data-secret'
 })
+
+// Only for debugging purpose
+// Will clear data when restart main process
+process.env.NODE_ENV === 'development'
+  && data.set('appProjects', [])
