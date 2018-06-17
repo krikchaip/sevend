9332131b03c19a7e7f011f3a84d7971027a76db9
krikchaip <krikchai.p@gmail.com>
2018-06-17 19:35:43 +0700

    👁 Projects : open project in explorer

--- src/renderer/components/ProjectsList.vue
+++ src/renderer/components/ProjectsList.vue
@@ -9,7 +9,7 @@
       <v-card>
         <v-list two-line>
           <v-list-tile
-            v-for="(project, idx) of $store.state.appProjects"
+            v-for="(data, idx) of $store.state.appProjects"
             :key="idx"
             @click=""
           >
@@ -17,10 +17,13 @@
               <v-icon class="blue lighten-2 white--text">mdi-docker</v-icon>
             </v-list-tile-avatar>
             <v-list-tile-content>
-              <v-list-tile-title>{{ project.project_name }}</v-list-tile-title>
-              <v-list-tile-sub-title>{{ project.project_path }}</v-list-tile-sub-title>
+              <v-list-tile-title>{{ data.project_name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ data.project_path }}</v-list-tile-sub-title>
             </v-list-tile-content>
             <v-list-tile-action>
+              <v-btn icon ripple @click="openFolder(data.project_path)">
+                <v-icon color="grey lighten-1">mdi-folder-open</v-icon>
+              </v-btn>
             </v-list-tile-action>
           </v-list-tile>
         </v-list>
@@ -30,10 +33,15 @@
 </template>
 
 <script>
+  import { shell } from 'electron'
+
   export default {
     name: 'ProjectsList',
     components: {
       ProjectsEmptyState: require('@/components/ProjectsEmptyState').default
+    },
+    methods: {
+      openFolder(path) { shell.openItem(path) }
     }
   }
 </script>
